d00983103f78f26799c5d824e68bc292503b5dea
Grzegorz Szymaszek <gszymaszek@short.pl>
2018-07-10 10:08:14 +0200

    Uproszczenie wyznaczania zwracanych wartości

--- rep_notes.py
+++ rep_notes.py
@@ -17,28 +17,21 @@ def get_addr_rep():
 def trusted_user(changeset):
     """Sprawdza czy user jest na liście zaufanych"""
     usr = changeset["user"]
-    if usr in th_usr:
-        return True
-    else:
-        return False
+
+    return usr in th_usr
 
 def trusted_app(changeset):
     """Sprawdza czy aplikacja jest na liście zaufanych"""
     app = changeset["tag"]["created_by"]
     check = [re.compile(a).match(app) for a in th_app ]
-    if True in check:
-        return True
-    else:
-        return False
 
+    return True in check
 
 def trusted(chs):
     """Pobiera dane changesetu i sprawdza listy zaufanych aplikacji i userów"""
     changeset = oapi.ChangesetGet(chs)
-    if trusted_user(changeset) and trusted_app(changeset):
-        return True
-    else:
-        return False
+
+    return trusted_user(changeset) and trusted_app(changeset)
 
 def create_comment(feat):
     """Tworzy konentarz na podstawie danych z raportu"""
