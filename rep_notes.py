299f025083435a4f76d3c2c188bbadc41b51012d
Grzegorz Szymaszek <gszymaszek@short.pl>
2018-07-10 12:32:20 +0200

    Zgodność rep_notes.py z domyślnymi regułami pylint

--- rep_notes.py
+++ rep_notes.py
@@ -1,15 +1,17 @@
 #! /usr/bin/env python
+# pylint: disable=C0111
 
-import psycopg2
+
 import re
-from conf import *
+import psycopg2
+import config
 
 
 def get_addr_rep():
     """Pobiera raport z bazy"""
-    with psycopg2.connect(conn_str) as conn:
+    with psycopg2.connect(config.conn_str) as conn:
         with conn.cursor() as cur:
-            cur.execute(rep_street_sql)
+            cur.execute(config.rep_street_sql)
             dbres = cur.fetchall()
     return dbres
 
@@ -18,45 +20,45 @@ def trusted_user(changeset):
     """Sprawdza czy user jest na liście zaufanych"""
     usr = changeset["user"]
 
-    return usr in th_usr
+    return usr in config.th_usr
+
 
 def trusted_app(changeset):
     """Sprawdza czy aplikacja jest na liście zaufanych"""
     app = changeset["tag"]["created_by"]
-    check = [re.compile(a).match(app) for a in th_app ]
+    check = [re.compile(a).match(app) for a in config.th_app]
 
     return True in check
 
+
 def trusted(chs):
     """Pobiera dane changesetu i sprawdza listy zaufanych aplikacji i userów"""
-    changeset = oapi.ChangesetGet(chs)
+    changeset = config.oapi.ChangesetGet(chs)
 
     return trusted_user(changeset) and trusted_app(changeset)
 
-def create_comment(feat):
+
+def create_comment(feat):  # pylint: disable=W0613
     """Tworzy konentarz na podstawie danych z raportu"""
     pass
 
 
-def post_comment(feat, comment):
+def post_comment(feat, comment):  # pylint: disable=W0613
     """Wysyła komentarz do OSM"""
     pass
-    # oapi.ChangesetComment(feat[1], comment)
-    # oapi.flush()
+    # config.oapi.ChangesetComment(feat[1], comment)
+    # config.oapi.flush()
+
 
 def rep_nostreet():
     """Główna pętla aplikacji"""
     rep = get_addr_rep()
-    # może już tu trzeba odfiltrować zaufanych a dopiero później po nich iterować?
+    # może już tu trzeba odfiltrować zaufanych a dopiero później po nich
+    # iterować?
     for feat in rep:
         if not trusted(feat[1]):
-            comm = create_comment(feat)
+            comm = create_comment(feat)  # pylint: disable=E1111
             post_comment(feat, comm)
 
 
 rep_nostreet()
-
-
-
-
-
