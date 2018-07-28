5f4f1ed4ce1f26a865bd47796779db9d654956ea
Andrzej Kępys <andrzej@abakus.net.pl>
2018-07-28 23:13:10 +0200

    Część druga w trakcie - dodany link pozwalający wczytać wszystkie uszkodzone do JOSM

--- webpage.py
+++ webpage.py
@@ -3,6 +3,20 @@ import locdb
 import config
 app = Flask(__name__)
 
+def create_josm_list(rep):
+    res_lst = list()
+    for feat in rep:
+        if feat["type"] == "node":
+            obj = "n"+str(feat["osm_id"])
+        elif feat["type"] == "way":
+            obj = "w"+str(feat["osm_id"])
+        elif feat["type"] == "relation":
+            obj = "r"+str(feat["osm_id"])
+        res_lst.append(obj)
+    return ",".join(res_lst)
+
+
+
 @app.route('/')
 def main_list():
     htdata = locdb.select_all(limit=100)
@@ -21,13 +35,15 @@ def main_list():
 def changeset_report(chgs_id):
     htdata = locdb.select_changeset(chgs_id)
     colnames = ["osm_id", "reason", "checks"]
+    all_obj = create_josm_list(htdata)
     return render_template("changeset.html",
                            rows=htdata,
                            colnames=colnames,
-                           reason_dict=config.reason_dict)
+                           reason_dict=config.reason_dict,
+                           all_obj=all_obj)
 
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="10.0.3.10", port="5000")
